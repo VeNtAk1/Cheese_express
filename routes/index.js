d3d7d44fe6e97699f67958c8c42845e9238ff9e1
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2024-11-21 22:30:55 +0300

    cheese_4_2_2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router();  
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/adugei', function(req, res, next) {
+  res.send("<h1>Адыгейский Сыр</h1>")
+});
 
+router.get('/chedder', function(req, res, next) {
+res.send("<h1>Чедский сыр</h1>")
+});
+router.get('/kandal', function(req, res, next) {
+res.send("<h1>Кандальский сыр</h1>")
+});
+router.get('/russian', function(req, res, next) {
+res.send("<h1>Русский сыр</h1>")
+});
+router.get('/tofy', function(req, res, next) {
+res.send("<h1>Тофский сыр</h1>")
+});
 module.exports = router;
