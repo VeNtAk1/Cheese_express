2137c43b7dcddd5e0c716147474b19566ed09af6
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2024-11-26 13:47:45 +0300

    cheese_7_2_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/testMongoose');
-const Cheese = mongoose.model('Cheese', { name: String });
-const cheesek = new Cheese({ name: 'Адыгейский сыр' });
-cheesek.save().then(() => console.log('СЫЫЫЫР'));
+mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
+var schema = mongoose.Schema({ name: String })
+schema.methods.meow = function(){
+console.log(this.name + " сказал мяу")
+}
+const Cat = mongoose.model('Cat', schema);
+const kitty = new Cat({ name: 'Пушок' });
+kitty.save().then(() => kitty.meow());git 
