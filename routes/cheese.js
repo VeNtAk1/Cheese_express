1d6e395ba6cf8b2605cd834de15e475dbe186e83
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2024-11-28 12:57:43 +0300

    cheese_8_3

--- routes/cheese.js
+++ routes/cheese.js
@@ -1,11 +1,22 @@
-var express = require('express');
+var express = require("express");
+const { Cheese } = require("../models/cheese");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с cheese');
+router.get("/", function (req, res, next) {
+  res.send("Новый маршрутизатор, для маршрутов, начинающихся с cheese");
 });
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-    });
+router.get("/:nick", async function (req, res, next) {
+  var cheeses = await Cheese.find({nick: req.params.nick});
+  console.log(cheeses);
+  if(!cheeses.length) return next(new Error("Нет такого сыра"))
+  var cheese = cheeses[0];
+  res.render('cheese', {
+  title: cheese.title,
+  picture: cheese.avatar,
+  desc: cheese.desc
+})
+
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var router = express.Router();  
+var router = express.Router(); 
+var Cheese = require("../models/cheese").Cheese; 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
