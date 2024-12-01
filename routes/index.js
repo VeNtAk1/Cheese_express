a5d38bdcdb53360b7df063e30ab6e3e3a35765a8
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2024-12-01 18:12:25 +0300

    cheese_10_7_2

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get("/logreg", function (req, res, next) {
   res.render("logreg", { title: "Вход", error: null });
 });
 
+
 /* POST login/registration page. */
 router.post("/logreg", async function (req, res, next) {
   var username = req.body.username;
@@ -39,41 +40,13 @@ router.post("/logreg", async function (req, res, next) {
     }
   }
 });
-router.get("/adugei", function (req, res, next) {
-  res.render("cheese", {
-    title: "Адыгейский Сыр",
-    picture: "images/adugei.png",
-    desc: "Традиционный кавказский сыр из коровьего молока, обладает насыщенным вкусом и плотной текстурой.",
-  });
+
+/* POST logout. */
+router.post('/logout', function(req, res, next) {
+  req.session.destroy();
+  res.locals.user = null;
+  res.redirect('/');
 });
 
-router.get("/chedder", function (req, res, next) {
-  res.render("cheese", {
-    title: "Чедский Сыр",
-    picture: "images/chedder.png",
-    desc: "Английский сыр из коровьего молока, известен своей мягкой текстурой и слегка острым вкусом.",
-  });
-});
-router.get("/kandal", function (req, res, next) {
-  res.render("cheese", {
-    title: "Канадльский Сыр",
-    picture: "images/kantal.png",
-    desc: "Нежный и мягкий сыр из цельного молока, подходит для салатов и закусок.",
-  });
-});
-router.get("/russian", function (req, res, next) {
-  res.render("cheese", {
-    title: "Российский Сыр",
-    picture: "images/russian.png",
-    desc: "Обобщенное название для различных видов сыров, производимых в России, включая мягкие, полутвёрдые и твёрдые сыры.",
-  });
-});
-router.get("/tofy", function (req, res, next) {
-  res.render("cheese", {
-    title: "Тофский Сыр",
-    picture: "images/tofy.png",
-    desc: "Сухой и соленый овечий сыр, традиционно изготавливаемый в Грузии.",
-  });
-});
 module.exports = router;
 //res.render('error');
