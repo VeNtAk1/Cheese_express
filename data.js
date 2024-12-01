de2178b373fdfa1fda38e401ef390dbc543012d2
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2024-12-01 14:55:46 +0300

    cheese_8_4_3

--- data.js
+++ data.js
@@ -1,30 +1,30 @@
 var data = [{
         title: 'Адыгейский Сыр',
-        nick: 'adugei sir',
+        nick: 'adugei',
         avatar: '/images/adugei.png',
         desc: 'Традиционный кавказский сыр из коровьего молока, обладает насыщенным вкусом и плотной текстурой.'
     },
     {
         title: 'Чедский Сыр',
-        nick: 'chedskij sir',
+        nick: 'chedder',
         avatar: '/images/chedder.png',
         desc: 'Английский сыр из коровьего молока, известен своей мягкой текстурой и слегка острым вкусом.'
     },
     {
         title: 'Канадльский Сыр',
-        nick: 'kandlskij sir',
+        nick: 'kandal',
         avatar: '/images/kantal.png',
         desc: 'Нежный и мягкий сыр из цельного молока, подходит для салатов и закусок.'
     },
     {
         title: 'Российский Сыр',
-        nick: 'russian sir',
+        nick: 'russian',
         avatar: '/images/russian.png',
         desc: 'Нежный и мягкий сыр из цельного молока, подходит для салатов и закусок.'
     },
     {
         title: 'Тофский Сыр',
-        nick: 'tofskij sir',
+        nick: 'tofy',
         avatar: '/images/tofy.png',
         desc: 'Cухой и соленый овечий сыр, традиционно изготавливаемый в Грузии.'
     }
